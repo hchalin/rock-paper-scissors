a58784b022f5dac5b22485bad1e964ccc6e86d4a
Hayden <Haydenchalin@gmail.com>
2021-06-14 08:33:47 -0400

    Created HTML file and sourced the JS to it. Made a Js file and started working on the 'Rock Paper Scissors; project to the best of my ability. Need to work on compareing two strings to see if they're equal in the playRound func

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rock-paper-scissors
 Rock, Paper, Scissors project for the odin project
+
+
---
+++ index.js
@@ -0,0 +1,56 @@
+//              Rock Paper Scissors Project
+
+
+//Returns a random answer for the computer
+function computerPlay() {
+    const answers = ["Rock", "Paper", "Scissors"];   
+    const answer = answers[Math.floor(Math.random() * answers.length)];
+    console.log(answer);
+    return answer;
+}
+
+
+//Variables
+const computerSelection = computerPlay();
+const playerSelection = prompt('Choose Rock, Paper, or Scissors');
+var playerAnswer = capitalize(playerSelection);
+
+
+
+
+
+//Capitalizes the Players answer
+function capitalize(string) {
+    string.toLowerCase();
+    return string[0].toUpperCase() + string.toLowerCase().slice(1);
+}
+console.log(capitalize(playerSelection));
+
+
+
+
+//plays one round of the game
+function playRound(playerAnswer, computerSelection) {
+    if (playerAnswer ===  "Rock") {
+        console.log("It's a tie! Go again!");
+    } else {
+        console.log("something");
+    }
+    
+  
+}
+playRound();
+
+ 
+
+
+/*  Write a function that plays a single round of the game.
+    The funtion should take 2 Parameters, "playerSelection", and "computerSelection"
+    It should return a string that declares the winner of the round
+    "You Lose, Rock beats Scissors!"
+    Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+
+        function capitalize(string) {
+            return string.charAt(0).toUpperCase() + string.splice(1);
+        }
+*/
