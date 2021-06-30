c2adf308e0c7d9b846bc0bc6c53d2f83ab841b62
Hayden <Haydenchalin@gmail.com>
2021-06-30 14:35:06 -0400

    Rearranged most of the funtions to get the game to work correctly. I should condense some of the console log statements so theres not 6 of them after each round.

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 //              Rock Paper Scissors Project
 
-
- 
 //Variables
-
+let round = 0;
 var playerScore = 0;
 var computerScore = 0;
-const computerSelection = computerPlay();
-const playerSelection = prompt('Choose Rock, Paper, or Scissors'); 
-var playerAnswer = capitalize(playerSelection);
+//const computerSelection = computerPlay();
+//const playerSelection = prompt('Choose Rock, Paper, or Scissors'); 
+
 
 //Returns a random answer for the computer
 function computerPlay() {
@@ -18,59 +16,60 @@ function computerPlay() {
     return answer;
 }
 
-
-
-//Capitalizes the Players answer
+function game(){
+    //computer select a answer 
+const computerSelection = computerPlay();
+    //player select a answer 
+const playerSelection = prompt('Choose Rock, Paper, or Scissors'); 
+var playerAnswer = capitalize(playerSelection);
+// capitalize the players answer
 function capitalize(string) {
     return string[0].toUpperCase() + string.slice(1).toLowerCase();
 }
 console.log("You: " + playerAnswer);
 
-//plays one round of the game
-function playRound(playerAnswer, computerSelection) {
-    if (playerAnswer === computerSelection) {
-        return "It's a tie!";
-    } else if (
-        playerAnswer === "Rock" && computerSelection === "Scissors" ||
-        playerAnswer === "Paper" && computerSelection === "Rock" ||
-        playerAnswer === "Scissors" && computerSelection === "Paper"
-    ) {
-        console.log("You gained a point");
-        return playerScore++;
-        
-    } else if (
-        playerAnswer === "Rock" && computerSelection === "Paper" ||
-        playerAnswer === "Paper" && computerSelection === "Scissors" ||
-        playerAnswer === "Scissors" && computerSelection === "Rock"
-    ) {
-        console.log("Computer gained a point!");
-        return computerScore++; 
-    } else (
-        console.log("Check your spelling.")
-    )
-};
-playRound(playerAnswer, computerSelection)
-console.log("You have " + playerScore + " points. The computer has " + computerScore + " points.");
-
-//create a function called game() that will loop through the playRound() function up to 5 and keep track of the score.
-
-function game() {
+    //decide who won 
+    function playRound(playerAnswer, computerSelection) {
+        if (playerAnswer === computerSelection) {
+            return "It's a tie!";
+        } else if (
+            playerAnswer === "Rock" && computerSelection === "Scissors" ||
+            playerAnswer === "Paper" && computerSelection === "Rock" ||
+            playerAnswer === "Scissors" && computerSelection === "Paper"
+        ) {
+            console.log("You gained a point");
+            return playerScore++;
+            
+        } else if (
+            playerAnswer === "Rock" && computerSelection === "Paper" ||
+            playerAnswer === "Paper" && computerSelection === "Scissors" ||
+            playerAnswer === "Scissors" && computerSelection === "Rock"
+        ) {
+            console.log("Computer gained a point!");
+            return computerScore++; 
+        } else (
+            console.log("Check your spelling.")
+        )
+    
+    };
+    // play the round 
     playRound(playerAnswer, computerSelection);
-    computerPlay();
-    playerSelection();
+    console.log("You: " + playerScore);
+    console.log("Computer: " + computerScore);
+    console.log("Round: " + round);
+    round++;
+    end(round);
+
+function end(round, playerScore, computerScore){
+    if (round === 5 && playerScore > computerScore){
+        console.log("You won")
+    } else if (
+        round === 5 && playerScore < computerScore
+    ) {
+        console.log('You lost')
+    } else {
+        return;
+    }
 }
 
-
-//make one function that calls the player & computer to select a answer, decide between the two who won and add points accordingly.
-
-function game(){
-    //computer select a answer 
-    const computerSelection = computerPlay();
-    //player select a answer 
-    const playerSelection = prompt('Choose Rock, Paper, or Scissors'); 
-    computerPlay()
-
-    //decide who won 
-    //add Points to who won
-    //when either the computer or player has five declare a winner
 }
